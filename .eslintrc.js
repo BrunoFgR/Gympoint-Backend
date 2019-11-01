d3c56ff1dbf46737fe84e3e97e95dcc0b3b1330f
BrunoFgR <brunofds281@gmail.com>
2019-11-01 17:38:45 -0300

    Configurando eslint e prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base', 
+    'eslint-config-prettier'
+  ],
+  plugins: [
+    'eslint-plugin-prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'camelcase': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePatters': 'next' }],
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({message: "Hello World"})
-})
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
+});
 
 export default routes;
