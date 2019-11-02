69266322296dfa3adfc74f5c519bef7350f08a8c
BrunoFgR <brunofds281@gmail.com>
2019-11-02 01:27:35 -0300

    Configurando middleware de autentificação

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
-import authConfig from '../../config/secret';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,12 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
