8c1455ef2b766c20ace892575d1bc2b0cbe1eb8d
BrunoFgR <brunofds281@gmail.com>
2019-11-02 15:39:41 -0300

    Validando dados de entrada

--- src/app/controllers/StudentsController.js
+++ src/app/controllers/StudentsController.js
@@ -1,8 +1,25 @@
+import * as Yup from 'yup';
 import Students from '../models/Students';
 import User from '../models/User';
 
 class StudentsController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .integer()
+        .required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails' });
+    }
+
     const user = await User.findOne({ where: { id: req.userId } });
 
     if (!user) {
@@ -29,6 +46,22 @@ class StudentsController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .integer()
+        .required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails' });
+    }
+
     const user = await User.findOne({ where: { id: req.userId } });
 
     if (!user) {
